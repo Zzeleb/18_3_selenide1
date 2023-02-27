9720d3d013960ba74386d14aa3a0d12aa2d25793
Artem Zholobov <zzeleb@gmail.com>
2023-02-27 20:55:15 +0400

    added test

---
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -0,0 +1,27 @@
+package github;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideRepositorySearch {
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+        // открыть главную страницу
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать enter
+        $("[placeholder='Search GitHub']").setValue("Selenide").pressEnter();
+        // кликнуть на первый репозиторий из списка найденных
+        $$("ul.repo-list li").first().$("a").click();
+        // проверка: заголовок selenide/selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+
+        sleep(5000);
+
+    }
+}
